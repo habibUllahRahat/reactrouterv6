e8f6730e917c7dcf157eba6adec2fd16e87909b4
habibUllahRahat <rahat09.hur@gmail.com>
2024-11-17 14:01:09 +0600

    Routing

--- src/main.jsx
+++ src/main.jsx
@@ -9,25 +9,27 @@ import Contact from "./routes/contact"
 import ErrorPage from "./routes/error-page"
 import Root from "./routes/root"
 
-// ! Handling error
-/**
-** If try to go to an path which dosen't exist it will show a default error pag
-** But we can set this Error page what ever we want
-** Lets create an Error page at src/error-page.jsx 
-// TODO: create src/error-page.jsx
+// ! Nested Routing
+/** 
+* ! Now it will call the Contact component inside the Root component but it won't show at first because we have to set the path where it will be called
+* * Now we have to set the path where this component will be called by using React Router DOM' sepcial component called <Outlet/> at src/routes/root.jsx
+
 */
 const router = createBrowserRouter([
 	//for multiple path routing we need to use array of an Object where each object represent a path
 	{
-		path: "/", //a path '/' represent which directory the elemnt or component wil be shown at browser link part
-		element: <Root />, //Here we willl set element or component to this correspondig path
-		errorElement: <ErrorPage />, // it will be shown if any error occur deuring rendering
+		path: "/",
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/contacts/:contactId",
+				element: <Contact />,
+				errorElement: <ErrorPage />,
+			},
+		],
 	},
-	{
-		path: "/contacts/:contactId", //a path '/' represent which directory the elemnt or component wil be shown at browser link part
-		element: <Contact />, //Here we willl set element or component to this correspondig path
-		errorElement: <ErrorPage />, // it will be shown if any error occur deuring rendering
-	},
+	,
 ])
 
 createRoot(document.getElementById("root")).render(
